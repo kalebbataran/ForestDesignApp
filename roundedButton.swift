ec09e32f88cd6d0b45dac19f619ceae41c011a8b
Kaleb Bataran <kalebb@kalebbataran.local>
2016-03-22 14:05:43 -0400

    adding files

---
+++ roundedButton.swift
@@ -0,0 +1,17 @@
+//
+//  roundedButton.swift
+//  DesignShowcase
+//
+//  Created by Kaleb Bataran on 3/8/16.
+//  Copyright © 2016 kaydot. All rights reserved.
+//
+
+import UIKit
+
+class roundedButton: UIButton {
+
+	override func awakeFromNib() {
+		layer.cornerRadius = 10.0
+	}
+
+}
